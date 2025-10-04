07528d7b6fdadb092f4b2d5254adf9ba94808132
Muthudhanush <dhanush@spearfox.com>
2025-10-04 13:17:28 +0530

    changes updated

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,41 @@
 /**
-    * @description      : 
-    * @author           : DHANUSH
-    * @group            : 
-    * @created          : 04/10/2025 - 12:28:44
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 04/10/2025
-    * - Author          : DHANUSH
-    * - Modification    : 
-**/
-import './App.css';
+ * @description      :
+ * @author           : DHANUSH
+ * @group            :
+ * @created          : 04/10/2025 - 12:28:44
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 04/10/2025
+ * - Author          : DHANUSH
+ * - Modification    :
+ **/
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="App">
+      <div className="content">
+        <h1>Counter App</h1>
+        <div>Count: {count}</div>
 
-      <h1>Counter App</h1>
-
+        <div className="actionButtons">
+          <button className="incrementButton" onClick={() => incrementCount()}>
+            Increment
+          </button>
+          <button className="decrementButton" onClick={() => decrementCount()}>
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
