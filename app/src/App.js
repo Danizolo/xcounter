f96aff212b01fdfd82e590c3db41a60e463c5221
Muthudhanush <dhanush@spearfox.com>
2025-10-04 12:40:15 +0530

    app created

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+/**
+    * @description      : 
+    * @author           : DHANUSH
+    * @group            : 
+    * @created          : 04/10/2025 - 12:28:44
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 04/10/2025
+    * - Author          : DHANUSH
+    * - Modification    : 
+**/
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Counter App</h1>
+
+    </div>
+  );
+}
+
+export default App;
